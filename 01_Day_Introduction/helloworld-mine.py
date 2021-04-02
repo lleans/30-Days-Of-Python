6ed9ea171cac93fd67c8c9617377d166d8861a67
lleans <ahmadunnail@gmail.com>
2021-04-02 08:25:45 +0700

    day 3

---
+++ 01_Day_Introduction/helloworld-mine.py
@@ -0,0 +1,19 @@
+# Python Day 1
+
+print(2+1) #penjumlahan
+print(3-1) #pengurangan
+print(3*2) #perkalian
+print(3/2) #pembagian
+print(3**2) #exponential
+print(3%2) #modulus
+print(3//2)
+
+
+print(type(10)) #int
+print(type(3.14)) #float
+print(type(1 + 3j)) #Complex number
+print(type("Python")) #String
+print(type([1, 2, 3])) #list
+print(type({'data': "Python"})) #Dictionary
+print(type({9.8, 3.2, 35})) #set
+print(type((9.8, 3.14, 2.5)))
---
+++ 02_Day_Variables_builtin_functions/variables-mine.py
@@ -0,0 +1,65 @@
+import math
+
+first_name = 'Python'
+last_name = '30-Days of python'
+full_name = 'Python 30-Days of python'
+country = 'Indonesian'
+city = 'Jakarta'
+age = 18
+year = 2021
+is_married = False
+is_true = True
+is_light_on = False
+name, long_name, nickname = 'lleans', "lleans's", "lleans"
+
+print(first_name, type(first_name))
+print(last_name, type(last_name))
+print(full_name, type(full_name))
+print(country, type(country))
+print(city, type(city))
+print(age, type(age))
+print(year, type(year))
+print(is_married, type(is_married))
+print(is_true, type(is_true))
+print(is_light_on, type(is_light_on))
+print(name, type(name))
+print(long_name, type(long_name))
+print(nickname, type(nickname))
+
+print("Length First Name: ", len(first_name))
+print("Length Last Name: ", len(last_name))
+
+num_one, num_two = 5, 4
+print(num_one, num_two)
+_total = num_one + num_two
+print(_total)
+_diff = num_two - num_one
+print(_diff)
+_product = num_two * num_one
+print(_product)
+_division = num_one / num_two
+print(_division)
+_reaminder =  num_two % num_one
+print(_reaminder)
+variable_exp = num_one ** num_two
+print(variable_exp)
+_floor_division = num_one // num_two
+print(_floor_division)
+
+# Circle
+circle_radius = 30
+area_of_circle = math.pi * circle_radius ** 2
+print(area_of_circle)
+circum_of_circle = 2 * math.pi * circle_radius
+print(circum_of_circle)
+
+# Circle-input
+circle_radius_input = int(input("jari-jari lingkaran: "))
+area_of_circle_input = math.pi * circle_radius_input ** 2
+print(area_of_circle_input)
+
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+country = input("Country: ")
+age = input("Age: ")
+print(first_name, last_name, country, age)
